9d133e1a56089fedc96d6766f97a26997b00091f
vanpeta <carlosallendesanz@gmail.com>
2017-05-24 14:07:38 -0700

    the reducer gets the payload of the action and send it to the container.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,12 @@
-export default function(state=null, action) {
+import { FETCH_WEATHER } from '../actions/index';
 
-
-	return state
+export default function(state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+		// instead using push, we want to use a pure function and don't mutate state inside a reducer, then we use concat which returns a new instance of state
+			//return state.concat([ action.payload.data ]);
+			//EJS6 syntax
+			return [ action.payload.data, ...state ];
+	}
+	return state;
 }
