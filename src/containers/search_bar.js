ea031fbd5e640369a501746c5a9b9a589d0eb9ea
vanpeta <carlosallendesanz@gmail.com>
2017-05-24 13:58:18 -0700

    adding axios to catch promises as middleware and resolve them before the action modifies state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
 
-export default class SearchBar extends Component {
+		this.state = { term: '' };
+
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({ term: event.target.value })
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' });
+	}
+
 	render() {
 		return (
-			<form className="input-group">
-				<input />
+			<form 
+				className="input-group"
+				onSubmit={this.onFormSubmit}
+				>
+				<input 
+					placeholder="Get a five-day forecast in your favorite city"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+					/>
 				<span className="input-group-btn">
 					<button type="submot" className="btn btn-secondary">
 						Submit
@@ -15,4 +44,10 @@ export default class SearchBar extends Component {
 		);
 	}
 }
+
+function mapDispathToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispathToProps)(SearchBar);
 
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,5 @@
+export default function(state=null, action) {
+
+
+	return state
+}
