ad9b04d433f9c9a634c6771c623aa369cef90717
vanpeta <carlosallendesanz@gmail.com>
2017-05-24 11:27:31 -0700

    searchbar component-container added

---
+++ src/containers/search_bar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class SearchBar extends Component {
+	render() {
+		return (
+			<form className="input-group">
+				<input />
+				<span className="input-group-btn">
+					<button type="submot" className="btn btn-secondary">
+						Submit
+					</button>
+				</span>
+			</form>
+		);
+	}
+}
+
